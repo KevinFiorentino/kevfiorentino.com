227e0bd1ff1722b5e5e9c7be3b01c14261bf9d96
Kevin <kevinfiorentino.1294@gmail.com>
2025-01-09 20:35:40 -0300

    Delete external-link, add new plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://kevfiorentino.com',
   integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks, // Usalo directamente sin cadena de texto
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+        }
+      ]
+    ]
+  },
   adapter: vercel()
 });
