5d30f9fee532b40b4ce14d36c8d1156aed340c1f
Kevin <kevinfiorentino.1294@gmail.com>
2025-01-09 21:22:01 -0300

    MDX plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,35 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from "@astrojs/vercel";
+import rehypeSlug from 'rehype-slug';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
+
+const icon = '<span class="copy"/>#</span>';
 
 export default defineConfig({
   site: 'https://kevfiorentino.com',
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     rehypePlugins: [
+      rehypeSlug,
       [
-        rehypeExternalLinks, // Usalo directamente sin cadena de texto
+        rehypeExternalLinks,
         {
           target: '_blank',
           rel: ['noopener', 'noreferrer'],
         }
+      ],
+      [
+        rehypeAutolinkHeadings, {
+          behavior: "apend",
+          content: fromHtmlIsomorphic(icon, {
+            fragment: true
+          }).children
+        }
       ]
-    ]
+    ],
   },
   adapter: vercel()
 });
