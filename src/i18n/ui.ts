cc7bb0ef258ad39b92b43f1d063d82fdb33bccc0
Kevin <kevinfiorentino.1294@gmail.com>
2025-02-28 17:04:48 -0300

    Change text

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -22,7 +22,7 @@ export const ui = {
 
     'home.kev': 'Hi, I\'m Kev!',
     'home.presentation1': 'Software engineer with more than 8 years of experience.',
-    'home.presentation2': 'I consider myself agnostic to the technology, with a generalist perspective. I am full-stack developer, specialized in Javascript, with a slight inclination towards front-end. I love Docker (DevOps is not my passion). I am good at using Figma... but not a good designer. Where I perform best is having an holistic vision of the software we are building, coordinating people, technology, and business.',
+    'home.presentation2': 'I consider myself agnostic to the technology, with a generalist perspective. I am full-stack developer, specialized in Javascript, with a slight inclination towards front-end. I love Docker (DevOps is not my passion). I am good at using Figma... but probably not the best designer. Where I perform best is having an holistic vision of the software we are building, coordinating people, technology, and business.',
     'home.presentation3': 'Throughout my career, I have worked with several own clients as a freelance. I worked for the public sector, on blockchain and IA projects, and in some startups. I founded and led my own startup, where I assumed the role of CTO and Team Manager, designing the architecture of a complex software and leading a full staff of developers for 2 years.',
     'home.presentation4': 'At this stage of my career, my priority is to meet people, belong and work on multidisciplinary teams, regardless of technology or project. I am looking to contribute my experience and continue developing professionally and personally.',
 
@@ -72,7 +72,7 @@ export const ui = {
 
     'home.kev': '¡Hola, soy Kev!',
     'home.presentation1': 'Ingeniero de software con más de 8 años de experiencia profesional.',
-    'home.presentation2': 'Me considero agnóstico a la tecnología, con una perspectiva generalista. Soy desarrollador full-stack, especializado en Javascript, con una leve inclinación hacia el front-end. Amo Docker (DevOps no es mi pasión). Soy bueno usando Figma... pero no buen diseñador. Donde mejor me desempeño es teniendo una visión holistica del software que estamos desarrollando, coordinando personas, tecnología y negocio.',
+    'home.presentation2': 'Me considero agnóstico a la tecnología, con una perspectiva generalista. Soy desarrollador full-stack, especializado en Javascript, con una leve inclinación hacia el front-end. Amo Docker (DevOps no es mi pasión). Soy bueno usando Figma... pero probablemente no el mejor diseñador. Donde mejor me desempeño es teniendo una visión holistica del software que estamos desarrollando, coordinando personas, tecnología y negocio.',
     'home.presentation3': 'A lo largo de mi carrera, He trabajado con varios clientes propios como freelance. Trabajé para el sector público, en proyectos blockchain e IA, y en algunas startups. Fundé y lideré mi propia startup, donde asumí el rol de CTO y Team Manager, diseñando la arquitectura de un complejo software y dirigiendo a un completo staff de desarrolladores durante 2 años.',
     'home.presentation4': 'En esta etapa de mi carrera, mi prioridad es conocer nuevas personas, pertenecer y trabajar en equipos multidisciplinarios, independientemente de la tecnología o el proyecto. Busco aportar mi experiencia y seguir desarrollándome profesional y personalmente.',
 
